c945660b41bf4bb69c8070979daae6fef9367ad1
vaibhavvanjole <vaibhavvanjole07@gmail.com>
2024-07-17 16:55:03 +0530

    second-change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginpageComponent
   ],
   imports: [
     BrowserModule,
